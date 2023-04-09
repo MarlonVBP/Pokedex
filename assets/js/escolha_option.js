1af4ff497f0805258ac264214402daf071c8cd3d
MarlonVoctorBezerradosPassos <marlonvicctor13@gmail.com>
2023-04-09 10:33:04 -0300

    lista_update

---
+++ assets/js/escolha_option.js
@@ -0,0 +1,4 @@
+function escolha_option(dado) {
+    // let pokemom = document.getElementById('input_pesquisa').value.toLowerCase();
+    console.log(dado);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-
 const buttom_pesquisa = document.getElementById('button_pesquisa');
+const buttom_pesquisa_modal = document.getElementById('button_pesquisar_modal');
 
 buttom_pesquisa.addEventListener('click', () => {
     iniciar();
@@ -11,53 +11,126 @@ document.addEventListener('keydown', (e) => {
 });
 
 function iniciar() {
-    // Chamando elementos do HTML
-    const img = document.getElementById('img_poke');
-    const nome = document.getElementById('nome');
-    const habilidade = document.getElementById('habilidade');
-    const forma = document.getElementById('formas');
-    const especie = document.getElementById('especie');
-    const tipo = document.getElementById('tipo');
-    const experiencia = document.getElementById('experiencia');
-    const altura = document.getElementById('altura');
-
     // Pegando o valor do input
     const pokemom = document.getElementById('input_pesquisa').value.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemom}`;
-    // Chamndo API
-    async function getItem(url) {
+    if (pokemom) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemom}`;
+
+        // Chamando elementos do HTML
+        const img = document.getElementById('img_poke');
+        const nome = document.getElementById('nome');
+        const habilidade = document.getElementById('habilidade');
+        const forma = document.getElementById('formas');
+        const especie = document.getElementById('especie');
+        const tipo = document.getElementById('tipo');
+        const experiencia = document.getElementById('experiencia');
+        const altura = document.getElementById('altura');
+
+        // Chamndo API
+        getItem(url, img, nome, habilidade, forma, especie, tipo, experiencia, altura)
+
+        // Limpando os spans
+        img.setAttribute('src', 'assets/imagens/pokemon-g4d28e9f6b_640.jpg');
+        nome.innerText = 'Desconhecido'
+        habilidade.innerText = ''
+        forma.innerText = ''
+        tipo.innerText = ''
+        especie.innerText = ''
+        experiencia.innerText = ''
+        altura.innerText = ''
+    }
+}
+
+function getItem(url, img, nome, habilidade, forma, especie, tipo, experiencia, altura) {
+    fetch(url)
+        .then(response => response.json())
+        .then(dados => {
+            console.log(dados);
+            nome.innerText = `${dados['name']}`
+            especie.innerText = `${dados['species']['name']}`
+            img.setAttribute('src', `${dados['sprites']['versions']['generation-v']['black-white']['animated'][`front_shiny`]}`);
+            experiencia.innerText = `${dados['base_experience']}`;
+            altura.innerText = `${dados['height']}`
+
+            dados.abilities.forEach((item) => {
+                habilidade.innerHTML += `${item['ability']['name']}<br>`
+            })
+
+            dados.forms.forEach((item) => {
+                forma.innerHTML += `${item['name']}<br>`
+            })
+
+            dados.types.forEach((item) => {
+                tipo.innerHTML += `${item['type']['name']}<br>`
+            })
+
+            // Limpando o input
+            document.getElementById('input_pesquisa').value = '';
+        })
+        .catch(_ => { console.log(_) })
+        .finally(() => { console.log('Processo finalizado!') });
+}
+
+function list_pokemons() {
+    let list = document.getElementById('list_pokemon')
+    let pokemon_name = []
+
+    for (let index = 1; index < 1011; index++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${index}`
+
         fetch(url)
             .then(response => response.json())
             .then(dados => {
-                console.log(dados);
-                nome.innerText = `${dados['name']}`
-                for (let i = 0, valido = `${dados['abilities']['length']}`; i < valido; i++) {
-                    habilidade.innerHTML += `${dados['abilities'][i]['ability']['name']}<br>`
-                }
-                forma.innerText = `${dados['forms']['0']['name']}`
-                especie.innerText = `${dados['species']['name']}`
-                for (let i = 0, valido = `${dados['types']['length']}`; i < valido; i++) {
-                    tipo.innerHTML += `${dados['types'][i]['type']['name']}<br>`
-                }
-                img.setAttribute('src', `${dados['sprites']['versions']['generation-v']['black-white']['animated'][`front_default`]}`);
-                experiencia.innerText = `${dados['base_experience']}`;
-                altura.innerText = `${dados['height']}`
-                // Limpando o input
-                document.getElementById('input_pesquisa').value = '';
+                pokemon_name.push(`${dados['name']}`)
             })
-            .catch(_ => { console.log(_) })
-            .finally(() => { console.log('Processo finalizado!') });
+            .catch(_ => { console.log(_) });
     }
-    getItem(url)
+    setTimeout(() => {
+        pokemon_name.sort()
 
-    // Limpando os spans
-    img.setAttribute('src', 'assets/imagens/pokemon-g4d28e9f6b_640.jpg');
-    nome.innerText = 'Desconhecido'
-    habilidade.innerText = ''
-    forma.innerText = ''
-    tipo.innerText = ''
-    especie.innerText = ''
-    experiencia.innerText = ''
-    altura.innerText = ''
+        pokemon_name.forEach((_item, index) => {
+            list.innerHTML += `
+            <option value="${pokemon_name[index]}" class="form-control option">${pokemon_name[index]}</option>
+            `;
+        })
+
+        exibir_escolha()
+    }, 2000);
 
 }
+let button_modal = document.getElementById('button_pesquisar_modal');
+let button_show_modal = document.getElementById('button_show_modal')
+
+button_modal.addEventListener('click', () => {
+    exiber_remove_show('remove')
+    escolha_option();
+})
+button_show_modal.addEventListener('click', () => {
+    exiber_remove_show('add')
+})
+
+function escolha_option() {
+    let pokemom = document.getElementById('input_pesquisa');
+    pokemom.value = document.querySelector('#list_pokemon').value;
+    setTimeout(()=>{
+        iniciar()
+    },500)
+}
+
+function exiber_remove_show(dado) {
+    if (dado == 'remove') {
+        list_modal = document.querySelector('.list_pokemon_modal');
+        list_modal.classList.remove('show');
+    } else if (dado == 'add') {
+        list_modal = document.querySelector('.list_pokemon_modal');
+        list_modal.classList.add('show');
+    }
+}
+
+function exibir_escolha() {
+    button_show_modal.classList.add('show')
+}
+
+
+list_pokemons()
+
